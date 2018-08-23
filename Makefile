ecae7cd2b915931455a616f69cb184f369e0487b
Navjot Kukreja <navjotk@gmail.com>
2018-08-22 22:14:34 -0500

    Fix bug in plotting

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ NO_WRITE_CP=--no-write-files
 
 CODE_FILES=run-intervals.sh async_experiment.py
 
+plots: plot_checkpoint_sizes.pdf plot_orders.pdf
+
 plot_checkpoint_sizes.pdf: results-2-1600.txt results-2-16000.txt results-2-160000.txt baseline-2.txt make_graph.py
 	python make_graph.py -f results-2-1600.txt 1600 -f results-2-16000.txt 16000 -f results-2-160000.txt 160000 -t "Slowdown attributed to checkpointing for differing checkpoint sizes" -x "interval" -b baseline-2.txt -o plot_checkpoint_sizes.pdf
 
--- make_graph.py
+++ make_graph.py
@@ -14,7 +14,7 @@ args = parser.parse_args()
 files = args.file
 title = args.title
 x_label = args.x
-baseline = args.baseline
+baseline = args.baseline[0]
 output_file = args.output_file
 
 assert(len(baseline) == 1 or len(baseline) == len(files))
@@ -27,9 +27,10 @@ for i, (filename, series_name) in enumerate(files):
     xs, ys = parse_data_file(filename, x_label)
     assert(base_xs == xs)
     change_ys = [(y-base_y)/base_y for y, base_y in zip(ys, base_ys)]
-    plt.plot(x, change_ys, label=series_name)
-plt.xlabel('Execution time (ms)')
-plt.ylabel('Interval')
+    plt.plot(xs, change_ys, label=series_name)
+plt.xlabel('Interval')
+plt.ylabel('Execution time (ms)')
+plt.legend(loc='lower right')
 plt.savefig(output_file, bbox_inches='tight')
     
     
